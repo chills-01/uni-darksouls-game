9ffda40ab73cced583118ae43271e835c95f12af
chil0005 <chil0005@student.monash.edu>
2021-09-15 11:17:49 +1000

    cemetery char now 'X', and now spawn undead

--- src/game/ground/Cemetery.java
+++ src/game/ground/Cemetery.java
@@ -1,9 +1,26 @@
 package game.ground;
 
 import edu.monash.fit2099.engine.Ground;
+import edu.monash.fit2099.engine.Location;
+import game.enemies.Undead;
+
 //todo add to gamemap in Application.java?
 public class Cemetery extends Ground {
     public Cemetery() {
-        super('C');
+        super('X');
     }
+
+    @Override
+    public void tick(Location location) {
+        // 25% chance of generating undead
+        int upperBound = 4;
+        int chance = new java.util.Random().nextInt(upperBound);
+
+        if (chance < 1) {
+            location.addActor(new Undead("Undead"));
+
+        }
+
+    }
+
 }
